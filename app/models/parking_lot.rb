55032148eec63e4998a9569575a1405d0944b1b4
mariamjaludi <mariam.jaludi@gmail.com>
2019-10-13 15:56:28 -0700

    method and association structure started

--- app/models/parking_lot.rb
+++ app/models/parking_lot.rb
@@ -1,2 +1,23 @@
 class ParkingLot < ApplicationRecord
+  # has_many :parking_floors
+  # has_many :parking_spots, through: :parking_floors
+  # has_one :parking_attendant_portal
+  # has_one :entrance_panel
+  # has_one :exit_panel
+
+
+  def add_parking_floor()
+    #return boolean
+  end
+
+  def add_enterance_panel()
+    #return boolean
+  end
+
+  def get_new_parking_ticket()
+    #return ParkingTicket
+  end
+
+  def isFull()
+  end
 end
